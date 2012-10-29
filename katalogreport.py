16eca3218266eb41be04dce45e0a094f8be9046c
Thomas.Meschede <Thomas.Meschede@ilr.tu-berlin.de>
2012-10-29 15:22:50 +0100

    ein Report wird jetzt herausgegeben

--- katalogreport.py
+++ katalogreport.py
@@ -8,6 +8,8 @@
 # modified:
 #	- 2012 10 25 - Thomas Meschede
 
+import sys
+
 from odspy import ods2table
 
 import numpy as np
@@ -45,3 +47,42 @@ for bs in bausteinetable[3:]:
     
   bausteine[bs[0]]={"Komponenten":bskomponenten,"Gewicht":gewicht}
   
+def listbsmass(bausteine):
+  bsmasslist=set((key, value["Gewicht"]) for key,value in bausteine.items())
+  return sorted(bsmasslist, key=lambda bs: -bs[1])
+
+def set2str(dt):
+  out=""
+  for line in dt:
+    out+="{:60}{:30.2f} kg\n".format(line[0],line[1])
+  return out
+
+def writereport():
+  report="""
+Bausteinreport:
+===============
+
+Bausteinmassen:
+---------------"""
+
+  report+="\n\n"+set2str(listbsmass(bausteine))
+  
+  return report
+
+def report2file(report):
+  reportfile=open("./Bausteinreport.txt","w")
+  reportfile.write(report)
+  reportfile.close()
+
+def main(argv=None):
+  if argv is None:
+    argv = sys.argv
+    try:
+      if argv[1]=="w": report2file(writereport())
+    except:
+      1+1
+    
+  print(writereport())
+
+if __name__ == "__main__":
+  main()
