251ee22c5b8264db72e25b6c9fa306e363d407c9
Thomas.Meschede <Thomas.Meschede@ilr.tu-berlin.de>
2012-11-26 16:49:25 +0100

    new script: optsat optimizes satellite configurations for iBoss

---
+++ optsat.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+# iboss-2
+# filename: optsat.py
+# author: - Thomas Meschede
+# 
+# usage: script optimizes a satellite and spits out result as XML
+#
+# modified:
+#       - created: 2012 11 26 - Thomas Meschede
+
+"""script loads an ods file into python arrays""" 
