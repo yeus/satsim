72b90f0a418bf6a91ff4fac520bc031ca3c8f673
Thomas.Meschede <Thomas.Meschede@ilr.tu-berlin.de>
2014-02-07 21:06:24 +0100

    added function from iboss_xml_load

--- python/iboss_catalogue.py
+++ python/iboss_catalogue.py
@@ -441,7 +441,7 @@ class Satellite(ibossxml):
   def bb(self):
     return self._bb
   @bb.setter
-  def bb(self,value): #todo remove this function
+  def bb(self,value): #TODO: remove this function and make a "normal" list variable out of it
     self._bb=value
   @bb.deleter
   def bb(self):
@@ -554,6 +554,67 @@ class Catalog(object):
     for vkeys,vvalues in self.sat.items():
       #missionen=ibosslist2xml("Satellites",referenzmissionen.values())
       self.savexml("bausteinkatalog/tub satellites/"+vkeys,vvalues.xml)
+
+  def loadxmldata(filename='bausteinkatalog/katalog.1.2.xml'):
+    data = et.parse(filename)
+    data = data.getroot()
+
+    co_list=data.find("components")
+    bs_list=data.find("buildingblocks")
+
+    #todo XML load routinen in iboss Klassen verschieben
+    components=dict()
+    for i in co_list:
+      new_co=iboss_catalogue.component(i.find("type").text)
+      for xmlprop in i:  #add properties
+        new_co.addxmlprop(xmlprop)
+      components[new_co.name]=new_co
+      
+    buildingblocks=dict()
+    for i in bs_list:
+      new_bs=iboss_catalogue.buildingblock(i.find("type").text)
+      for xmlprop in i:
+        if xmlprop.tag=="components":
+          for co in xmlprop:
+            new_co=copy.copy(components[co.attrib["type"]])
+            if "num" in co.attrib: new_co.num=int(co.attrib["num"])
+            for co_prop in co:
+              #new_co.addxmlprop(co_prop) #Hier immer Ergänzungen aus dem laden einer alten katalogdatei hinzufügen
+              if co_prop.tag=="pos": new_co.pos=iboss_catalogue.ibossxml.xml2vec(co_prop)*pq.Quantity(1,"blocks")
+              if co_prop.tag=="th_vec": new_co.th_vec=iboss_catalogue.ibossxml.xml2vec(co_prop)*pq.dimensionless
+            new_bs.add_co(new_co)
+        else: new_bs.addxmlprop(xmlprop)
+      buildingblocks[new_bs.name]=new_bs
+
+    mission=dict()
+    
+    return components, buildingblocks, mission
+
+  def loadmission(filename):  
+    co,bb,mn=loadxmldata()
+    data = et.parse(filename)
+    xmlmission = data.getroot()
+
+    mission=iboss_catalogue.mission(data.find("type").text)
+    for xmlprop in xmlmission:
+      if xmlprop.tag=="buildingblocks":
+        for xmlbb in xmlprop:
+          newbb=copy.copy(bb[xmlbb.attrib["type"]])
+          mission.add_bb(bb=newbb,pos=iboss_catalogue.str2vecint(xmlbb.attrib["pos"]),rot=iboss_catalogue.str2vec(xmlbb.attrib["rot"])*pq.deg)
+      else: mission.addxmlprop(xmlprop)
+      
+    return mission
+
+  def test():
+    import saveasxml
+    saveasxml.Version="test"
+    saveasxml.saveibosslists(co,bb,ms) #todo: implement as unittest
+
+    #calculate power demands for buildingblocks
+    for BB in bb.values():
+      BB.update()
+      #print(u"{}: {}".format(BB.name,BB.power))  #python2.7 Version
+      print("{}: {}".format(BB.name,BB.power))
 
 # Bei Abhängigkeiten zwischen Objekten (normalerweise müsste aber alles funktionieren)
 #http://stackoverflow.com/questions/6376081/pickle-linked-objects
