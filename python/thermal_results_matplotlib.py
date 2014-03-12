4ffd7d7545dc34bdab68b7f75091550ffb03a4bb
Jens <jens.riesselmann@ilr.tu-berlin.de>
2014-03-12 11:40:30 +0100

    Im Script wird jetzt über die Schaltzusrände der Heizer auch summiert

--- python/thermal_results_matplotlib.py
+++ python/thermal_results_matplotlib.py
@@ -24,8 +24,13 @@ fig, TIM_TSS_dT = plt.subplots()
 fig, Panel_T = plt.subplots()
 fig, Radiation_qflow = plt.subplots()
 fig, heater_on = plt.subplots()
+fig, sum_heater_on = plt.subplots()
 fig, cooler_on = plt.subplots()
-a = np.genfromtxt('E:\generic_satellite_3x3x3_sun_res.csv', delimiter=',',names=True)
+a = np.genfromtxt('E:\sim_results\sat_3x3x3\omnidirectional_radiation\generic_satellite_3x3x3_sun_res.csv', delimiter=',',names=True,dtype=float)
+summe = np.zeros(len(np.array(a)))
+print(len(np.array(a)))
+print(type(a))
+print(summe.size)
 i=1
 while i<=BBx:
     j=1
@@ -42,6 +47,7 @@ while i<=BBx:
             while i_c<=len(component_heater_cooler):
                 heater_on.plot(a['time'],a["BB"+str(i)+str(j)+str(k)+str(component_heater_cooler[i_c-1])+str(physical_quantity[4])],label="BB"+str(i)+str(j)+str(k))
                 cooler_on.plot(a['time'],a["BB"+str(i)+str(j)+str(k)+str(component_heater_cooler[i_c-1])+str(physical_quantity[5])],label="BB"+str(i)+str(j)+str(k))
+                summe = summe + a["BB"+str(i)+str(j)+str(k)+str(component_heater_cooler[i_c-1])+str(physical_quantity[4])]
                 i_c=i_c+1
             i_c=1
             while i_c<=len(component_TIM):
@@ -92,7 +98,7 @@ while i<=BBx:
         j=j+1
     i=i+1
 
-
+sum_heater_on.plot(a['time'],summe,label="BB"+str(i)+str(j)+str(k))
 #summe=np.array(a['BB222port_bT'])+np.array(a['BB222port_bT'])
 #ax.plot(a['t'],summe)
 el_Box_T.legend(loc='upper right', shadow=True)
