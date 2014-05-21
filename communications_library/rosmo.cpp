0f06f784709bee2f09c9e646133053df7ab8f131
Thomas.Meschede <Thomas.Meschede@ilr.tu-berlin.de>
2014-05-21 14:10:02 +0200

    modelica test itnerface for external libraries

--- communications_library/rosmo.cpp
+++ communications_library/rosmo.cpp
@@ -5,3 +5,33 @@
 //  modified:
 //        - 2014 02 27 - Thomas Meschede 
 // testing interfacing c code with modelica
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+// *****************************************************************************
+// *****************************************************************************
+// *****************************************************************************
+
+/* file: ExternalFunc1.c */
+double ExternalFunc1_ext(double x)
+{
+  double res;
+  res = x+2.0*x*x;
+  return res;
+}
+
+/* file: ExternalFunc2.c */
+double ExternalFunc2(double x)
+{
+  double res;
+  res = (x-1.0)*(x+2.0);
+  return res;
+}
+
+void test() { 
+  cout << "testblabla";
+}
--- communications_library/rosmo.h
+++ communications_library/rosmo.h
@@ -5,3 +5,25 @@
 //  modified:
 //        - 2014 02 27 - Thomas Meschede 
 // testing interfacing c code with modelica
+
+
+#ifdef __cplusplus
+#define EXTERNC extern "C"
+#else
+#define EXTERNC
+#endif
+
+//  typedef void* mylibrary_mytype_t;
+// 
+//  EXTERNC mylibrary_mytype_t mylibrary_mytype_init();
+//  EXTERNC void mylibrary_mytype_destroy(mylibrary_mytype_t mytype);
+//  EXTERNC void mylibrary_mytype_doit(mylibrary_mytype_t self, int param);
+
+EXTERNC void test();
+
+/* Header file ExternalFunc1_ext.h for ExternalFunc1 function */
+EXTERNC double ExternalFunc1_ext(double);
+/* Header file ExternalFunc2.h for ExternalFunc2 */
+EXTERNC double ExternalFunc2(double);
+
+#undef EXTERNC
