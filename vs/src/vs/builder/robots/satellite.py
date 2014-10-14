eb93386d2b1386b9ce944a2d6fc6aae590302a82
Thomas.Meschede <Thomas.Meschede@ilr.tu-berlin.de>
2014-10-14 14:38:01 +0200

    first version of generic satellite

--- vs/src/vs/builder/robots/satellite.py
+++ vs/src/vs/builder/robots/satellite.py
@@ -1,14 +1,40 @@
 from morse.builder import *
 
+from morse.core import blenderapi
+import mathutils
+from mathutils import *
+
 class Satellite(GroundRobot):
     """
     A template robot model for satellite, with a motion controller and a pose sensor.
+    
+    #builds the robot in the blender scene BEFORE the actual game environment
     """
     def __init__(self, name = None, debug = True):
 
         # satellite.blend is located in the data/robots directory
-        GroundRobot.__init__(self, 'vs/robots/satellite.blend', name)
+        Robot.__init__(self, 'vs/robots/satellite.blend', name)
         self.properties(classpath = "vs.robots.satellite.Satellite")
+
+        #select layer to put the satellite into:
+        #bpy.data.scenes['Scene'].layers=[False]*19+[True]
+        #import packages from Satellite blender file TODO: move this into default "scene file" or find another solution with importing python files or something similar
+        from genutils import copyobject as cpobj
+        from arrows import arrow
+        
+        
+        for i in range(10):
+          newobj=cpobj("baustein")
+          newobj.location=Vector([i*2.0,0,0])
+          newobj.rotation_mode="XYZ"
+
+        #sce = blenderapi.bge.logic.getCurrentScene()
+        #obj_list = sce.objects
+        
+        #con=blenderapi.bge.logic.getCurrentController()
+        #own = con.owner
+        #obj=sce.addObject("Cone", own)
+        #obj.localPosition=(0,0,0)
 
         ###################################
         # Actuators
@@ -18,19 +44,19 @@ class Satellite(GroundRobot):
         # (v,w) motion controller
         # Check here the other available actuators:
         # http://www.openrobots.org/morse/doc/stable/components_library.html#actuators
-        self.motion = MotionVW()
-        self.append(self.motion)
+        #self.motion = MotionVW()
+        #self.append(self.motion)
 
-        # Optionally allow to move the robot with the keyboard
-        if debug:
-            keyboard = Keyboard()
-            keyboard.properties(ControlType = 'Position')
-            self.append(keyboard)
+        ## Optionally allow to move the robot with the keyboard
+        #if debug:
+            #keyboard = Keyboard()
+            #keyboard.properties(ControlType = 'Position')
+            #self.append(keyboard)
 
-        ###################################
-        # Sensors
-        ###################################
+        ####################################
+        ## Sensors
+        ####################################
 
-        self.pose = Pose()
-        self.append(self.pose)
+        #self.pose = Pose()
+        #self.append(self.pose)
 
--- vs/src/vs/robots/satellite.py
+++ vs/src/vs/robots/satellite.py
@@ -1,6 +1,9 @@
 import logging; logger = logging.getLogger("morse." + __name__)
 import morse.core.robot
 
+from morse.core import blenderapi
+from genutils import copyobject as cpobj
+
 class Satellite(morse.core.robot.Robot):
     """ 
     Class definition for the satellite robot.
@@ -14,6 +17,8 @@ class Satellite(morse.core.robot.Robot):
         Receives the reference to the Blender object.
         Optionally it gets the name of the object's parent,
         but that information is not currently used for a robot.
+        
+        #gets executed within the blender game engine environment
         """
 
         logger.info('%s initialization' % obj.name)
@@ -21,6 +26,34 @@ class Satellite(morse.core.robot.Robot):
 
         # Do here robot specific initializations
         logger.info('Component initialized')
+        
+        #sce = blenderapi.bge.logic.getCurrentScene()
+        #obj_list = sce.objects
+        
+        #con=blenderapi.bge.logic.getCurrentController()
+        #own = con.owner
+        #obj=sce.addObject("Cone", own)
+        #obj.localPosition=(0,0,0)
+        
+        #main simulation file
+        #from itertools import product
+
+
+        #print("start simulation")
+
+
+        ##sim=bge.sim
+
+        #dt=1.0/bge.logic.getLogicTicRate()
+
+        #for x,y,z in product(range(-3,1),range(-3,3), range(-3,3)):
+            #print("adding object")
+            #obj=sce.addObject("baustein.transparent", own)
+            ##obj.applyRotation((0,ay,az),1) 
+            ##obj.setParent(sv.blobj)
+
+    def get_buildingblocks(self):
+      return None
 
     def default_action(self):
         """ Main loop of the robot
